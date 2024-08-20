daf294d387b1a2961d76439a85a2118c9a5f50c5
lxx <418763253@qq.com>
2024-08-20 10:57:29 +0800

    release

--- packages/core/build/vite.es.config.ts
+++ packages/core/build/vite.es.config.ts
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import { readdirSync, readdir } from 'fs'
-import { delay, defer, filter, map } from 'lodash-es'
-import { visualizer } from 'rollup-plugin-visualizer'
+import { defineConfig } from 'vite' // 导入配置的定义函数
+import { resolve } from 'path' // 导入解析路径的方法
+import { readdirSync, readdir } from 'fs' // 导入读取目录的方法
+import { delay, defer, filter, map } from 'lodash-es' // 导入工具函数
+import { visualizer } from 'rollup-plugin-visualizer' // 可视化分析依赖的模块
 
-import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
-import shell from 'shelljs'
-import { hooksPlugin as hooks } from '@lxx-ui/vite-plugins'
-import terser from '@rollup/plugin-terser'
+import vue from '@vitejs/plugin-vue' // 导入在vite中使用的vue插件
+import dts from 'vite-plugin-dts' // 导入生成类型文件的插件
+import shell from 'shelljs' // 脚本执行工具
+import { hooksPlugin as hooks } from '@lxx-ui/vite-plugins' // 导入自定义的钩子函数
+import terser from '@rollup/plugin-terser' // 压缩插件
 
-const TRY_MOVE_STYLES_DELAY = 800 as const
+const TRY_MOVE_STYLES_DELAY = 800 as const // 常量，延迟移动样式文件
 
-const isProd = process.env.NODE_ENV === 'production'
-const isDev = process.env.NODE_ENV === 'development'
-const isTest = process.env.NODE_ENV === 'test'
+const isProd = process.env.NODE_ENV === 'production' // 常量，是否是生产环境
+const isDev = process.env.NODE_ENV === 'development' // 常量，是否是开发环境
+const isTest = process.env.NODE_ENV === 'test' // 常量，是否是测试环境
 
+// 获取给定目录下的目录集合
 function getDirectoriesSync(basePath: string) {
   const entries = readdirSync(basePath, { withFileTypes: true })
 
@@ -24,7 +25,7 @@ function getDirectoriesSync(basePath: string) {
     (entry) => entry.name
   )
 }
-
+// 移动目录
 function moveStyles() {
   readdir('./dist/es/theme', (err) => {
     if (err) return delay(moveStyles, TRY_MOVE_STYLES_DELAY)
@@ -32,6 +33,7 @@ function moveStyles() {
   })
 }
 
+// 配置选项
 export default defineConfig({
   plugins: [
     vue(),
